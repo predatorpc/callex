c0ae3d28c436565859d6e8759e6ad4af31488225
bymaks <34max01@gmail.com>
2017-06-26 08:10:25 +0700

    nikita

--- views/reports/calls.php
+++ views/reports/calls.php
@@ -5,7 +5,9 @@ use yii\helpers\ArrayHelper;
 use app\models\CommentsActions;
 use app\models\CommentsTypes;
 use app\models\CallStatuses;
-
+use yii\helpers\Url;
+use kartik\date\DatePicker;
+use yii\widgets\ActiveForm;
 $this->title = 'Отчет по звонкам';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
@@ -13,11 +15,43 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h1><?= Html::encode($this->title) ?></h1>
 
+    <div class="calendar-fast">
+        <a class="dashed" href="<?=Url::to(['reports/calls', 'dateStart' => Date("Y-m-d"), 'dateEnd' => Date("Y-m-d")]);?>">Сегодня</a>|
+        <a class="dashed" href="<?=Url::to(['reports/calls', 'dateStart' => Date('Y-m-d', strtotime('-1 day')), 'dateEnd' => Date('Y-m-d', strtotime('-1 day'))]);?>">Вчера</a>|
+        <a class="dashed" href="<?=Url::to(['reports/calls', 'dateStart' => Date('Y-m-d', strtotime('-2 day')), 'dateEnd' => Date('Y-m-d', strtotime('-2 day'))]);?>">Позавчера</a>|
+        <a class="dashed" href="<?=Url::to(['reports/calls', 'dateStart' => Date('Y-m-d', strtotime('-1 week')), 'dateEnd' => Date('Y-m-d')]);?>">Прош. неделя</a>|
+        <a class="dashed" href="<?=Url::to(['reports/calls', 'dateStart' => Date('Y-m-d', strtotime('-1 month')), 'dateEnd' => Date("Y-m-d")]);?>">Прош. месяц</a>
+    </div>
+    <?php $form = ActiveForm::begin(['action' => '/reports/calls', 'id' => 'forum_post', 'method' => 'get',]); ?>
+    <?= DatePicker::widget([
+        'name' => 'dateStart',
+            'value' => isset($_GET['dateStart']) ? $_GET['dateStart'] : '',
+            'pluginOptions' => [
+                'autoclose'=>true,
+                'format' => 'dd.mm.yyyy'
+            ]
+        ]);
+       ?>
+    <?= DatePicker::widget([
+        'name' => 'dateEnd',
+        'value' => isset($_GET['dateStart']) ? $_GET['dateEnd'] : '',
+        'pluginOptions' => [
+            'autoclose'=>true,
+            'format' => 'dd.mm.yyyy'
+        ]
+    ]);
+    ?>
+        <div class="form-group">
+            <?= Html::submitButton('-->', ['class' => 'btn btn-success']) ?>
+        </div>
+        <?php ActiveForm::end(); ?>
     <?php
     $layoutGrid= '
         <div> {toolbar}</div>
+        {summary}
         {items}
         <div class="clearfix"></div>
+        {pager}
         ';
     ?>
     <?= GridView::widget([
